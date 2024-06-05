a19bbf5b042d3d41dddecbc0ace18c19be66efc9
zif-01 <159900483+zif-01@users.noreply.github.com>
2024-06-06 02:30:23 +0700

    Update RecordRepository.java
    методы поиска по полям в интерфейс

--- src/main/java/com/example/cache/repository/RecordRepository.java
+++ src/main/java/com/example/cache/repository/RecordRepository.java
@@ -3,5 +3,10 @@ package com.example.cache.repository;
 import com.example.cache.model.Record;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RecordRepository extends CrudRepository<Record, String> {
+    Optional<Record> findByAccount(long account);
+    Optional<Record> findByName(String name);
+    Optional<Record> findByValue(double value);
 }
